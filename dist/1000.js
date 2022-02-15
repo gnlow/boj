277a59cb448620368a6b3482f1a41b16f889270d
Gnlow <gnlow@protonmail.com>
2022-02-15 19:25:14 +0900

    delete dist

--- dist/1000.js
+++
@@ -1,9 +0,0 @@
-// deno-fmt-ignore-file
-// deno-lint-ignore-file
-// This code was bundled using `deno bundle` and it's not recommended to edit it manually
-
-function main(input) {
-    const [[a, b]] = input.split("\n").map((x)=>x.split(" ").map(Number)
-    );
-}
-console.log(main(require("fs").readFileSync("/dev/stdin") + ""));
