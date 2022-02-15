23ed23d54f8feb4ecd7112db1e9836c7ffbf60f3
Gnlow <gnlow@protonmail.com>
2022-02-15 18:46:32 +0900

    use `Deno.emit`

--- bundle.ts
+++ bundle.ts
@@ -10,17 +10,14 @@ targets.forEach(
                 console.log(main(require("fs").readFileSync("/dev/stdin")+""))
             `
         )
-        const process = Deno.run({
-            cmd: [
-                "deno",
-                "bundle",
-                `temp/${target}.ts`,
-                `dist/${target}.js`,
-                "--no-check",
-            ],
-            stdout: "piped"
-        })
-        await process.output()
-        process.close()
+        const emitResult = await Deno.emit(
+            `temp/${target}.ts`,
+            {
+                bundle: "module",
+                check: false,
+            }
+        )
+        const bundled = emitResult.files["deno:///bundle.js"]
+        await Deno.writeTextFile(`dist/${target}.js`, bundled)
     }
 )
