f92aacc0e0a46ad204c80c4afff3892e12d37e9f
Gnlow <gnlow@protonmail.com>
2022-02-16 13:52:43 +0900

    add cli/make

---
+++ cli/make.ts
@@ -0,0 +1,20 @@
+const targets = Deno.args
+
+const defaultCode = 
+`import { flow } from "fp/function.ts"
+import { simplify } from "util/simplify.ts"
+
+export const main = flow(
+    
+)`
+
+targets.forEach(
+    async target => {
+        const path = `problem/${target}/`
+        await Deno.mkdir(path)
+        await Promise.all([
+            Deno.writeTextFile(path + "main.ts", defaultCode),
+            Deno.writeTextFile(path + "test.yaml", "")
+        ])
+    }
+)
