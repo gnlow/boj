35015044cd9aa7cc5ab6d8216c0d6aab1703564a
Gnlow <gnlow@protonmail.com>
2022-02-17 22:14:45 +0900

    solve 1546

---
+++ problem/1546/main.ts
@@ -0,0 +1,25 @@
+import { flow } from "fp/function.ts"
+import { simplify } from "util/simplify.ts"
+import { bimap } from "fp/Tuple.ts"
+import { create } from "fp/std/Tuple.ts"
+import { map } from "fp/Array.ts"
+import { mean } from "fp/std/Array.ts"
+import { fromArray, max } from "fp/NonEmptyArray.ts"
+import { Ord } from "fp/number.ts"
+import * as O from "fp/Option.ts"
+
+export const main = flow(
+    simplify,
+    ([
+        [_n],
+        [...as],
+    ]) => as,
+    fromArray,
+    as => create([as, as]),
+    bimap(
+        O.map(max(Ord)),
+        O.map(mean),
+    ),
+    map(O.getOrElse(() => 0)),
+    ([meanV, m]) => meanV / m * 100
+)
