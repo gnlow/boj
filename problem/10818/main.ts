fa4b6b6c8f116cbb339bf889950cb0296ca58b4b
Gnlow <gnlow@protonmail.com>
2022-02-18 15:17:57 +0900

    solve 10818

---
+++ problem/10818/main.ts
@@ -0,0 +1,26 @@
+import { pipe, flow } from "fp/function.ts"
+import { simplify } from "util/simplify.ts"
+import * as O from "fp/Option.ts"
+import { toArray } from "fp/ReadonlyArray.ts"
+import { fromArray, min, max, flap, NonEmptyArray } from "fp/NonEmptyArray.ts"
+import { Ord } from "fp/number.ts"
+
+const assertNonEmpty = 
+    (ns: number[]) =>
+    pipe(
+        ns,
+        fromArray,
+        O.getOrElse(() => [0] as NonEmptyArray<number>),
+    )
+
+export const main = flow(
+    simplify,
+    ([_, ns]) => ns,
+    toArray,
+    assertNonEmpty,
+    ns => flap(ns)([
+        min(Ord),
+        max(Ord),
+    ]),
+    ([a, b]) => a + " " + b,
+)
