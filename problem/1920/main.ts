1d565f92555b4d36bb700eebf1a30301d5f82df5
Gnlow <gnlow@protonmail.com>
2022-02-19 11:14:09 +0900

    fail 1920

--- problem/1920/main.ts
+++ problem/1920/main.ts
@@ -1,14 +1,45 @@
-import { flow, pipe } from "fp/function.ts"
-import { simplifyNs } from "util/simplify.ts"
-import { map } from "fp/ReadonlyArray.ts"
-import { join } from "fp/std/ReadonlyArray.ts"
+import { flow } from "fp/function.ts"
+import { split } from "fp/string.ts"
+
+function* lazySplit(str: string) {
+    let result = ""
+    for (const char of str) {
+        if (char == " ") {
+            yield result
+            result = ""
+        } else {
+            result += char
+        }
+    }
+    yield result
+}
 
 export const main = flow(
-    simplifyNs,
-    ([_n, ns, _m, ms]) => [ns, ms],
-    ([ns, ms]) => pipe(
-       ms,
-       map(m => +ns.includes(m)+""),
-    ),
-    join("\n"),
+    split("\n"),
+    ([_0, ns, _1, ms]) => {
+        const nsStack: string[] = []
+        const lazyNs = lazySplit(ns)
+        const lazyMs = lazySplit(ms)
+        let result = ""
+        let nsDone = false
+        for (const m of lazyMs) {
+            result += (() => {
+                if (!nsDone) {
+                    while (!nsStack.includes(m)) {
+                        const nextNs = lazyNs.next()
+                        if (nextNs.done) {
+                            nsDone = true
+                            return "0"
+                        } else {
+                            nsStack.push(nextNs.value)
+                        }
+                    }
+                } else {
+                    return nsStack.includes(m) ? "1" : "0"
+                }
+                return "1"
+            })() + "\n"
+        }
+        return result
+    }
 )
