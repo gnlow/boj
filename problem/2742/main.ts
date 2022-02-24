a4eeda58a9e0a9820463d46c632ee5bc50bc19a4
Gnlow <gnlow@protonmail.com>
2022-02-24 16:03:40 +0900

    solve 2742

---
+++ problem/2742/main.ts
@@ -0,0 +1,13 @@
+import { flow } from "fp/function.ts"
+import { simplifyNs } from "util/simplify.ts"
+import { range, map, reverse } from "fp/NonEmptyArray.ts"
+import { join } from "fp/std/Array.ts"
+
+export const main = flow(
+    simplifyNs,
+    ([[n]]) => n,
+    n => range(1, n),
+    map(String),
+    reverse,
+    join("\n"),
+)
