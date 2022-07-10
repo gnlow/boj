ee215cd4870975031dabf4d3b7f57694e295e8b9
Gnlow <gnlow@protonmail.com>
2022-07-10 10:04:15 +0900

    fix 1676/golf

--- problem/1676/golf.js
+++ problem/1676/golf.js
@@ -1,2 +1,1 @@
-n=require("fs").readFileSync(0)
-console.log(Array(3).fill().map((_,i)=>5**i*5).map(x=>~~(n/x)).reduce((p,c)=>p+c,0))
+console.log(~~((n=require("fs").readFileSync(0))/5)+~~(n/25)+~~(n/125))
